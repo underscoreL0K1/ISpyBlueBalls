50a96d9e79856d1bde445611d695e2dbdb68d486
Loki <71656278+underscoreL0K1@users.noreply.github.com>
2022-03-01 09:41:28 -0500

    Update iRecieveBlueBalls.bat

--- iRecieveBlueBalls.bat
+++ iRecieveBlueBalls.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Users\faken\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\faken\PycharmProjects\Vision2\templates\iRecieveBlueBalls.py"
+"C:\Users\faken\AppData\Local\Programs\Python\Python310\python.exe" "iRecieveBlueBalls.py"
 pause
